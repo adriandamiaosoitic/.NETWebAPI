447dc0f8881da5b152e26da2e8171621005527fa
adriandamiaosoitic <86964732+adriandamiaosoitic@users.noreply.github.com>
2021-07-21 13:56:23 -0300

    Aula 18 (#2)

--- Controllers/DirectorController.cs
+++ Controllers/DirectorController.cs
@@ -2,13 +2,14 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebAPIProject.Models;
 
 [ApiController] // Diz que a classe Controller é uma API
 [Route("[controller]")] // Rota do recurso
 public class DirectorController : ControllerBase
 {
     private readonly ApplicationDbContext _context; //Contexto intermediario para comunicação com o banco
-    public DirectorController(ApplicationDbContext context)
+    public DirectorController(ApplicationDbContext context) // Injeção de dependência -> pra construir um controller é necessario uma classe de contexto
     {
         _context = context;
     }
@@ -21,25 +22,41 @@ public class DirectorController : ControllerBase
          return Ok(director);
     }
 
+    //Get All by ID
     [HttpGet]
     public async Task<List<Director>> Get()
     {
         return await _context.Directors.ToListAsync();
     }
+
+    //GetByID
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Director>> Get(long id)
+    {
+        var director = await _context.Directors.FirstOrDefaultAsync(director => director.Id == id);
+        return Ok(director);
+    }
         
 
-    [HttpPut]
-    public string Put()
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Director>> Put(long id, [FromBody] Director director)
     {
-        return "Put";
+        director.Id = id;
+        _context.Directors.Update(director);
+        await _context.SaveChangesAsync();
+
+        return Ok(director);
     }
 
     
-
-    [HttpDelete]
-    public string Delete()
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(long id)
     {
-        return "Delete";
+        var director = await _context.Directors.FirstOrDefaultAsync(director => director.Id == id);
+        _context.Remove(director);
+        await _context.SaveChangesAsync();
+        return Ok(director);
     }
+
 
 }
--- Models/Director.cs
+++ Models/Director.cs
@@ -6,7 +6,7 @@ public class Director{
     public string Name { get; set; }
     public ICollection<Movie> Movies { get; set; }
 
-    public Director(string name){
+    public Director(string name){ //Construido para poder inicializar uma lista de filmes
         Name = name;
         Movies = new List<Movie>();
     }
